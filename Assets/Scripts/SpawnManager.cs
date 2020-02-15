f9bce9ff04e5ff41cedfec9711cbeb863ef0e4f3
catonice <catonicedeveloper@gmail.com>
2020-02-15 14:13:05 +0200

    Assets

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+
+    private bool _stopSpawning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var coroutine = SpawnRoutine();
+        StartCoroutine(coroutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Spawn enemy GameObjects every 5 seconds
+    // IEnumerator allows us to use Yield events which waits
+    private IEnumerator SpawnRoutine() 
+    {
+        // yield return null; // Wait 1 frame
+        
+        // Infinite loop inside coroutine
+        while (!_stopSpawning) 
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7f, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+
+            // Assign to enemy container to clean up game hierarchy
+            newEnemy.transform.parent = _enemyContainer.transform;
+
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    public void OnPlayerDeath() 
+    {
+        _stopSpawning = true;
+    }
+}
